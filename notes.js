905f88a14e520815f455320756d1825b88d08f10
Lisa Williams <lisaismyname0@gmail.com>
2022-05-31 12:56:30 -0400

    made 4x3 grid in html using flexbox

---
+++ notes.js
@@ -0,0 +1,7 @@
+// given an array of objects (customers.js)
+// given an array of states (states.js)
+
+// will need to make javascript and html
+// priority is data, not style
+// have existing html, and will populate html with data from our javascript
+// this is our first database... a collection of structured data
