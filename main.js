3d4aa166e32038cab763d13145c5539e53fa57fa
Lisa Williams <lisaismyname0@gmail.com>
2022-06-01 13:16:37 -0400

    Complete? names formatted, some confusion on why certain divs ignore css rules but styling is not the focus here.

--- main.js
+++ main.js
@@ -1,8 +1,21 @@
-
 const containerElement = document.querySelector("#container")
 
 for (let customer of customers) {
-
+    // the following chunk of code is a (longwinded) way of formatting the customer names
+    const firstTitle = customer.name.title.charAt(0);
+    const upperTitle = firstTitle.toUpperCase()
+    const titleRemain = customer.name.title.slice(1);
+    const title = `${upperTitle}${titleRemain}`
+    const firstFirst = customer.name.first.charAt(0);
+    const upperFirst = firstFirst.toUpperCase()
+    const firstRemain = customer.name.first.slice(1);
+    const firstName = `${upperFirst}${firstRemain}`;
+    const firstLast = customer.name.last.charAt(0);
+    const upperLast = firstLast.toUpperCase()
+    const lastRemain = customer.name.last.slice(1);
+    const lastName = `${upperLast}${lastRemain}`
+    const fullName = `${title} ${firstName} ${lastName}`
+    // now I begin to create my elemements
     let cardElement = document.createElement("div")
     cardElement.classList.add("card")
 
@@ -18,30 +31,30 @@ for (let customer of customers) {
 
     let customerElement = document.createElement("h2")
     customerElement.classList.add("customer")
-    customerElement.innerText = `Customer Name: ${customer.name.title} ${customer.name.first} ${customer.name.last}`
+    customerElement.innerText = `Customer Name: ${fullName}`
     cardElement.appendChild(customerElement)
 
     let emailElement = document.createElement("h3")
     emailElement.classList.add("emails")
-    emailElement.innerText = `Email Address: ${customer.email}`
+    emailElement.innerText = `Email Address: ${customer.email} `
     cardElement.appendChild(emailElement)
 
     let addressElement = document.createElement("p")
     addressElement.classList.add("towns")
     const state = nameToAbbr(customer.location.state)
-    addressElement.innerText = `Location: ${customer.location.street.number} ${customer.location.street.name}, ${customer.location.city} ${state} ${customer.location.postcode}`
+    addressElement.innerText = `Location: ${customer.location.street.number} ${customer.location.street.name}, ${customer.location.city} ${state} ${customer.location.postcode} `
     cardElement.appendChild(addressElement)
 
     let dobElement = document.createElement("p")
     dobElement.classList.add("dates")
     let dobFormat = moment(customer.dob.date).format("MMM DD YYYY");
-    dobElement.innerText = `DOB: ${dobFormat} Age: ${customer.dob.age}`
+    dobElement.innerText = `DOB: ${dobFormat} Age: ${customer.dob.age} `
     cardElement.appendChild(dobElement)
 
     let registeredElement = document.createElement("p")
     registeredElement.classList.add("registration")
     let registeredFormat = moment(customer.registered.date).format("MMM DD YYYY")
-    registeredElement.innerText = `Date Registered: ${registeredFormat} Registered at age: ${customer.registered.age}`
+    registeredElement.innerText = `Date Registered: ${registeredFormat} Registered at age: ${customer.registered.age} `
     cardElement.appendChild(registeredElement)
 
     containerElement.appendChild(cardElement)
@@ -55,29 +68,6 @@ for (let customer of customers) {
     // console.log(`Address: ${customer.location.street.number} ${customer.location.street.name} ${customer.location.city} ${customer.location.state} ${customer.location.postcode}`);
     // console.log(`Customer Registered on: ${customer.registered.date} at ${customer.registered.age} years old`)
 }
-
-// TODO:
-// format customer names
-
-for (let customer of customers) {
-    const first = customer.name.title.charAt(0)
-    console.log(first)
-
-    // const upper = first.toUpperCase
-    // console.log(upper)
-
-    // let remain = 
-
-    // const combined = upper + remain;
-
-    // console.log(combined)
-
-    // dobForrmat
-
-
-}
-
-
 
 
 
