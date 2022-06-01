1dd45143207784d06ee7a864e771effe30bc977e
Lisa Williams <lisaismyname0@gmail.com>
2022-06-01 13:56:42 -0400

    Data complete, styling closer resembles example

--- main.js
+++ main.js
@@ -16,12 +16,8 @@ for (let customer of customers) {
     const lastName = `${upperLast}${lastRemain}`
     const fullName = `${title} ${firstName} ${lastName}`
     // now I begin to create my elemements
-    let cardElement = document.createElement("div")
+    let cardElement = document.createElement("article")
     cardElement.classList.add("card")
-
-    let contentElement = document.createElement("div")
-    contentElement.classList.add("square")
-    contentElement.appendChild(cardElement)
 
     let imageElement = document.createElement("img")
     imageElement.src = customer.picture.large;
@@ -29,32 +25,32 @@ for (let customer of customers) {
     imageElement.classList.add("photos")
     cardElement.appendChild(imageElement)
 
-    let customerElement = document.createElement("h2")
+    let customerElement = document.createElement("h1")
     customerElement.classList.add("customer")
-    customerElement.innerText = `Customer Name: ${fullName}`
+    customerElement.innerText = `${fullName}`
     cardElement.appendChild(customerElement)
 
-    let emailElement = document.createElement("h3")
+    let emailElement = document.createElement("span")
     emailElement.classList.add("emails")
-    emailElement.innerText = `Email Address: ${customer.email} `
+    emailElement.innerText = `Email Address: ${customer.email}`
     cardElement.appendChild(emailElement)
 
-    let addressElement = document.createElement("p")
+    let addressElement = document.createElement("span")
     addressElement.classList.add("towns")
     const state = nameToAbbr(customer.location.state)
     addressElement.innerText = `Location: ${customer.location.street.number} ${customer.location.street.name}, ${customer.location.city} ${state} ${customer.location.postcode} `
     cardElement.appendChild(addressElement)
 
-    let dobElement = document.createElement("p")
+    let dobElement = document.createElement("span")
     dobElement.classList.add("dates")
     let dobFormat = moment(customer.dob.date).format("MMM DD YYYY");
-    dobElement.innerText = `DOB: ${dobFormat} Age: ${customer.dob.age} `
+    dobElement.innerText = `DOB: ${dobFormat}`
     cardElement.appendChild(dobElement)
 
-    let registeredElement = document.createElement("p")
+    let registeredElement = document.createElement("span")
     registeredElement.classList.add("registration")
     let registeredFormat = moment(customer.registered.date).format("MMM DD YYYY")
-    registeredElement.innerText = `Date Registered: ${registeredFormat} Registered at age: ${customer.registered.age} `
+    registeredElement.innerText = `Date Registered: ${registeredFormat}`
     cardElement.appendChild(registeredElement)
 
     containerElement.appendChild(cardElement)
